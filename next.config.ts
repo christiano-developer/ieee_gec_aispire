db5f3dc916c8ffdb357e396412ed477bb919452d
Christiano Blairoy Fernandes <88627133+christiano-developer@users.noreply.github.com>
2026-04-14 02:28:20 +0530

    Add basePath and assetPrefix to next.config.ts
    addressing basePath misconfiguration

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/ieee_gec_aispire",
+  assetPrefix: "/ieee_gec_aispire/",
+
   images: {
     unoptimized: true,
   },
 
-  trailingSlash: true, 
+  trailingSlash: true,
 };
 
 export default nextConfig;
